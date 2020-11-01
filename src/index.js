4fbb45bfc27c2021326ca63fa294486736fbd21a
Khanh Hung <hung@swagsoft.com>
2020-11-01 18:03:24 +0700

    cmit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// make bluebird default Promise
+Promise = require('bluebird'); // eslint-disable-line no-global-assign
+const app = require('./config/express');
+const mongoose = require('./config/mongoose');
+
+// open mongoose connection
+mongoose.connect();
+
+// listen to requests
+// app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+app.listen(process.env.PORT || 3002);
+
+/**
+* Exports express
+* @public
+*/
+module.exports = app;
